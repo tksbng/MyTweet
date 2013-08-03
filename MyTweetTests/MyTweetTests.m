3872a77fe6904e3ac0b0b0de30e03ef54e1a3655
Takeshi Bingo <bingotakeshi@Takeshi-no-MacBook-Air.local>
2013-08-03 13:06:22 +0900

    Initial Commit

---
+++ MyTweetTests/MyTweetTests.m
@@ -0,0 +1,32 @@
+//
+//  MyTweetTests.m
+//  MyTweetTests
+//
+//  Created by Takeshi Bingo on 2013/08/03.
+//  Copyright (c) 2013年 Takeshi Bingo. All rights reserved.
+//
+
+#import "MyTweetTests.h"
+
+@implementation MyTweetTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MyTweetTests");
+}
+
+@end
